937c4d17cbae1a9582f1466c25489f483e9101b3
zhangtao <zhangtao103239@163.com>
2018-05-27 22:09:15 +0800

    获取天气结果，并打印

--- src/cn/ynotme/weather/Main.java
+++ src/cn/ynotme/weather/Main.java
@@ -1,8 +1,16 @@
 package cn.ynotme.weather;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 	// write your code here
+        Document document = Jsoup.connect("http://www.weather.com.cn/data/cityinfo/101010100.html")
+                .get();
+        System.out.println(document.body().text());
     }
 }
